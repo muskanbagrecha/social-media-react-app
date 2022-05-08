7a0118c1b3082f69bc6e80f7b87ffdc219fa701a
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-05-08 10:34:56 +0530

    feat: created folder structure

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -11,5 +11,5 @@ const firebaseConfig = {
   measurementId: "G-DDQM2NSRRE",
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
